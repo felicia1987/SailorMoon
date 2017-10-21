7b6a552753ed1c9bfdeea76d57f326a3844b6af1
felicia1987 <32760651+felicia1987@users.noreply.github.com>
2017-10-22 04:56:17 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,84 +1,120 @@
-/* webcoder assignment JavaScript */
+/* webcoder assignment CSS */
 
-// Header Scroll
-$(window).on('scroll', function() {
-    var scroll = $(window).scrollTop();
+	// Header Scroll
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
 
-    if (scroll >= 50) {
-        $('.navbar-default').addClass('modifyTemp');
-        $('#header').addClass('modifyTemp');
-    } else {
-        $('.navbar-default').removeClass('modifyTemp');
-        $('#header').removeClass('modifyTemp');
-    }
-});
+		if (scroll >= 50) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');
+		}
+	});
 
-// on submitting the form
+	// Fancybox
+	$('.myThumbnail-box').fancybox();
+
+
+	// Page Scroll
+	var sections = $('section')
+		nav = $('nav[role="navigation"]');
+
+	$(window).on('scroll', function () {
+	  	var cur_pos = $(this).scrollTop();
+	  	sections.each(function() {
+	    	var top = $(this).offset().top - 76
+	        	bottom = top + $(this).outerHeight();
+	    	if (cur_pos >= top && cur_pos <= bottom) {
+	      		nav.find('a').removeClass('active');
+	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    	}
+	  	});
+	});
+	nav.find('a').on('click', function () {
+	  	var $el = $(this)
+	    	id = $el.attr('href');
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - 75
+		}, 500);
+	  return false;
+	});
+
+	// Mobile Navigation
+	$('.nav-toggle').on('click', function() {
+		$(this).toggleClass('close-nav');
+		nav.toggleClass('open');
+		return false;
+	});	
+	nav.find('a').on('click', function() {
+		$('.nav-toggle').toggleClass('close-nav');
+		nav.toggleClass('open');
+	});
+
+    // on submitting the form
 $('form').submit(function (event) {
-            
-    // prevent the default action of reloading the page
-    event.preventDefault();
     
-    var sendData = {};
-    $(event.target.nodeName + ' :input').each(function(){
-        console.log(this.type);
-        if (this.type == "radio") {
-            if (this.checked == true) {
-                sendData[this.name] = $(this).val();
-            }
-        } else {
-            sendData[this.name] = $(this).val(); 
-        }
-            
-        console.log(this);                
-    });
-    
-    var posting = $.ajax({
-        type: 'POST',
-        url: $(event.target.nodeName).prop('action'),
-        data: sendData
-    });
-    
-    posting.done(function (response) {
-        console.log(response);
+// prevent the default action of reloading the page
+event.preventDefault();
 
-        $('form :input').each(function(){
-                    
-            if ($(this).prop('type') != 'submit') {  // Clear the value of all input elements EXCEPT submit button & radio!
-                if($(this).prop('type') != 'radio'){ 
-                    $(this).val('');
-                }                        
-            }
-                else {
-                    $(this).prop('disabled', true);     // Set the button to disabled again since form will be empty
-                }
-            });
-
-            $('#alert-id').removeClass("in").addClass("show");        
-        });
-        
-    posting.fail(function (response) {
-        console.log(response);
-    });
+var sendData = {};
+$(event.target.nodeName + ' :input').each(function(){
+console.log(this.type);
+if (this.type == "radio") {
+    if (this.checked == true) {
+        sendData[this.name] = $(this).val();
+    }
+} else {
+    sendData[this.name] = $(this).val(); 
+}
+    
+console.log(this);                
 });
-    
+
+var posting = $.ajax({
+type: 'POST',
+url: $(event.target.nodeName).prop('action'),
+data: sendData
+});
+
+posting.done(function (response) {
+console.log(response);
+
+$('form :input').each(function(){
+            
+    if ($(this).prop('type') != 'submit') {  // Clear the value of all input elements EXCEPT submit button & radio!
+        if($(this).prop('type') != 'radio'){ 
+            $(this).val('');
+        }                        
+    }
+        else {
+            $(this).prop('disabled', true);     // Set the button to disabled again since form will be empty
+        }
+    });
+
+    $('#alert-id').removeClass("in").addClass("show");        
+});
+
+posting.fail(function (response) {
+console.log(response);
+});
+});
+
 // RESPONSE ALERT WINDOW-------------------------------------------------------------------------------
 /* include the following HTML to use:
 <div class="form-group">
-    <button type="submit" class="btn btn-default my-btn form-control" id="submit-id">submit</button>                   
-    <div class="alert alert-danger alert-dismissible fade in" hidden id="alert-id">
-        <button type="button" class="close" id="close-id"><span>&times;</span></button>
-            Thank you! We will send you update whenever it's available.
-    </div>
+<button type="submit" class="btn btn-default my-btn form-control" id="submit-id">submit</button>                   
+<div class="alert alert-danger alert-dismissible fade in" hidden id="alert-id">
+<button type="button" class="close" id="close-id"><span>&times;</span></button>
+    Thank you! We will send you update whenever it's available.
+</div>
 </div>
 */
-    
+
 // on clicking the X button
 $('#close-id').click(function(){
-    // hide the alert panel by changing the class
-    $('#alert-id').removeClass("show").addClass("in");
+// hide the alert panel by changing the class
+$('#alert-id').removeClass("show").addClass("in");
 
-    // enable the form
-    $('form :input').prop('disabled', false);  
+// enable the form
+$('form :input').prop('disabled', false);  
 });
-
